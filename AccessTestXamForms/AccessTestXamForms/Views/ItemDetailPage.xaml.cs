f4f5a2d6e7d47e19a8eff3d45e4da116b6a34fc4
Carlos Trujillo Gamez <Carlos.TrujilloGamez@ato.gov.au>
2020-07-15 15:10:20 +1000

    Add files

---
+++ AccessTestXamForms/AccessTestXamForms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AccessTestXamForms.Models;
+using AccessTestXamForms.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AccessTestXamForms.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        private ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+
+        private async System.Threading.Tasks.Task Button_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
